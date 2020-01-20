44ccc50cb17569483978aca8ca3eeb11bbd7eaac
Bobson Lin <bobson801104@gmail.com>
2020-01-20 11:03:05 +0800

    feat: 🎸 Add dev-packages and flask serve CLI for livereload

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import logging
 import cognitive_face as MS_CF
 
 from flask import Flask, request, redirect, session, render_template, send_from_directory
+from livereload import Server
 from utils import export_ms_result_images
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
-file_handler = logging.FileHandler('debug.log')
-file_handler.setFormatter(logging.Formatter(
-    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
-))
-app.logger.addHandler(file_handler)
+# file_handler = logging.FileHandler('debug.log')
+# file_handler.setFormatter(logging.Formatter(
+#     '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+# ))
+# app.logger.addHandler(file_handler)
 
 
 @app.route('/', methods=['GET'])
@@ -21,7 +22,7 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    target = os.path.join(app.root_path, 'images/')
+    target = os.path.join(app.root_path, 'images')
 
     if not os.path.isdir(target):
         os.mkdir(target)
@@ -54,7 +55,7 @@ def analyze():
         MS_CF.Key.set(os.getenv('MS_KEY'))
     else:
         raise AssertionError('Please set your Microsoft Face API Key')
-    
+
     if os.getenv('MS_URL'):
         MS_CF.BaseUrl.set(os.getenv('MS_URL'))
     else:
@@ -67,5 +68,12 @@ def analyze():
     export_ms_result_images(session['destination'], faces)
     return render_template('result.html',
                            image_name='{}_result{}'.format(
-                               os.path.splitext(session['filename'])[0],
-                               os.path.splitext(session['filename'])[1]))
+                                os.path.splitext(session['filename'])[0],
+                                os.path.splitext(session['filename'])[1]))
+
+
+@app.cli.command('serve', help='Run a live reload server for development.')
+def serve_app():
+    server = Server(app.wsgi_app)
+    # server.watch
+    server.serve()
